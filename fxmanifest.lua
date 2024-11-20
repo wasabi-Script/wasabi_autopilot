32924a618830269d7847a49f52b4618b658fc2b8
wasabi-Script <oneuseritsme@gmail.com>
2024-11-20 20:47:22 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+description 'Auto Pilot'
+
+author 'wasabirobby'
+
+version '1.12'
+
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
